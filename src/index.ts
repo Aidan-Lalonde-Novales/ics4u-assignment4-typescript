3402558060c3366dc14f29dc8f50fe0aeaf1d47f
Aidan-Lalonde-Novales <aidan.lalonde.novales@mths.ca>
2022-11-22 01:50:31 +0000

    done

--- src/index.ts
+++ src/index.ts
@@ -1,126 +1,49 @@
 /**
- * This program generates numbers for a magic square.
+ * This program generates an hourglass.
  *
  * By:      Aidan Lalonde-Novales
  * Version: 1.0
- * Since:   2022-11-20
+ * Since:   2022-11-21
  */
 
-const MAGIC_NUM = 15
-const POSSIBLE_NUM = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+import promptSync from 'prompt-sync'
+const prompt = promptSync()
 
 /**
- * Check For Duplicates.
+ * This function prints an hourglass (out of *)
  *
- * @param {number[]} sqrArray - array to be checked.
- * @returns {boolean} T/F for if sqrArray has duplicates or not.
+ * @param {number} stars - current amount of stars to be added
+ * @param {number} spaces - current amount of spaces to be added
  */
-function hasDuplicates(sqrArray: number[]): boolean {
-  const sortedSqrArray = sqrArray.slice().sort(function (a, b) {
-    return a - b
-  })
+function hourglass(stars: number, spaces: number): void {
+  // variable to be outputed each recursion
+  let currentOutput = ''
 
-  const results = []
-  for (let count = 0; count < sortedSqrArray.length - 1; count++) {
-    if (sortedSqrArray[count + 1] === sortedSqrArray[count]) {
-      results.push(sortedSqrArray[count])
+  // base case
+  if (stars !== 0) {
+    // add spaces
+    for (let count = 0; count < spaces; count++) {
+      currentOutput += ' '
     }
-  }
-  return results.length !== 0
-}
-
-/**
- * Check if Square is Magic.
- *
- * @param {number[]} sqrArray - array to be checked.
- * @returns {boolean} T/F for if sqrArray is magic or not.
- */
-function isMagic(sqrArray: number[]): boolean {
-  if (hasDuplicates(sqrArray)) {
-    return false
-  } else {
-    // define rows
-    const row1 = sqrArray[0] + sqrArray[1] + sqrArray[2]
-    const row2 = sqrArray[3] + sqrArray[4] + sqrArray[5]
-    const row3 = sqrArray[6] + sqrArray[7] + sqrArray[8]
-    // define columns
-    const col1 = sqrArray[0] + sqrArray[3] + sqrArray[6]
-    const col2 = sqrArray[1] + sqrArray[4] + sqrArray[7]
-    const col3 = sqrArray[2] + sqrArray[5] + sqrArray[8]
-    // define diagonals
-    const diag1 = sqrArray[0] + sqrArray[4] + sqrArray[8]
-    const diag2 = sqrArray[2] + sqrArray[4] + sqrArray[6]
-
-    return (
-      row1 === row2 &&
-      row2 === row3 &&
-      row3 === col1 &&
-      col1 === col2 &&
-      col2 === col3 &&
-      col3 === diag1 &&
-      diag1 === diag2 &&
-      diag2 === MAGIC_NUM
-    )
-  }
-}
-
-/**
- * Magic Square Printing Function.
- *
- * @param {number[]} arr - array to be printed.
- */
-function printSquare(arr: number[]): void {
-  console.log(
-    `${arr[0]}` +
-      ' ' +
-      `${arr[1]}` +
-      ' ' +
-      `${arr[2]}` +
-      '\n' +
-      `${arr[3]}` +
-      ' ' +
-      `${arr[4]}` +
-      ' ' +
-      `${arr[5]}` +
-      '\n' +
-      `${arr[6]}` +
-      ' ' +
-      `${arr[7]}` +
-      ' ' +
-      `${arr[8]}` +
-      '\n'
-  )
-}
-
-/**
- * Generates Magic Squares.
- *
- * @param {number[]} pNum - possible numbers to be added to a magic square.
- * @param {number[]} sqrArray - array to be filled with pNum.
- * @param {number} index - current index of sqrArray to add pNum to.
- */
-function generateSquare(
-  pNum: number[],
-  sqrArray: number[],
-  index: number
-): void {
-  // prints valid magic squares
-  if (index === 9 && isMagic(sqrArray)) {
-    printSquare(sqrArray)
-  } else {
-    // run through each number for each index
-    if (index !== 9) {
-      for (let count = 0; count < 9; count++) {
-        sqrArray[index] = pNum[count]
-        generateSquare(pNum, sqrArray, index + 1)
-      }
+    // add stars
+    for (let count = 0; count < stars; count++) {
+      currentOutput += '* '
     }
+    console.log(currentOutput)
+    hourglass(stars - 1, spaces + 1)
+    console.log(currentOutput)
   }
 }
 
-const sqrArray: number[] = []
+// main stub
+const userInput = prompt('How Tall Would You Like Your Hourglass? ')
+const userInt = parseInt(userInput)
 
-console.log('Generating All 3x3 Magic Squares...\n')
-generateSquare(POSSIBLE_NUM, sqrArray, 0)
+console.log('')
+if (isNaN(userInt) || userInt <= 0) {
+  console.log('Invalid Input.')
+} else {
+  hourglass(userInt, 0)
+}
 
-console.log('Done.')
+console.log('\nDone.')
